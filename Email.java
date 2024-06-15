fe5f84771d82229ae37962a53e0fcab2ec802f2e
Julian Sanchez <juliansg30@gmail.com>
2024-06-15 09:04:44 -0500

    1° commit creacion de clase

---
+++ Email.java
@@ -0,0 +1,18 @@
+package udemy;
+
+/*
+* Programa el cual genera un user de email
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Generador_Email {
+
+    public static void main(String[] args) {
+        
+        
+    }
+
+  
+}
